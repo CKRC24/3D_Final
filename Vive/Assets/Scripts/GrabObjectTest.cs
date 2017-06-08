80f60ecd511ebe83c2ec5ca42ee29e19a0a773c3
DESKTOP-N96HJ5K\User <brian8311@gmail.com>
2017-06-08 20:35:48 +0800

    Initialize Vive Unity Project

---
+++ Vive/Assets/Scripts/GrabObjectTest.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabObjectTest : MonoBehaviour
+{
+
+    private SteamVR_TrackedObject trackedObj;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // the object that the controller collides with
+    private GameObject collidingObject;
+
+    // the object that is current in hand
+    private GameObject objectInHand;
+
+    private void SetCollidingObject(Collider col)
+    {
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+        collidingObject = col.gameObject;
+    }
+
+    // 1
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 2
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 3
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+    private void grabObject()
+    {
+        // put the object in hand
+        objectInHand = collidingObject;
+        collidingObject = null;
+
+        // add joint on the object
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+
+        // make sure the joint wont break easily
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    private void ReleaseObject()
+    {
+        if (GetComponent<FixedJoint>())
+        {
+            // remove connection and destroy the joint
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+
+            // add speed and rotation when release
+            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+        objectInHand = null;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        // grab object when Hair Trigger Down
+        if (Controller.GetHairTriggerDown())
+        {
+            if (collidingObject)
+            {
+                grabObject();
+            }
+        }
+
+        // throw object when Hair Trigger Up
+        if (Controller.GetHairTriggerUp())
+        {
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+    }
+}
